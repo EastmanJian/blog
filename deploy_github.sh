c8eae68a5519f462d08997ee39b551369b02294a
Eastman Jian <50935@qq.com>
2017-05-05 02:05:37 +0800

    deploy blog

---
+++ deploy_github.sh
@@ -0,0 +1,9 @@
+cp -r _site/ /tmp/
+git checkout gh-pages
+rm -r ./*
+cp -r /tmp/_site/* ./
+rm -r /tmp/_site/
+git add -A
+git commit -m "deploy blog"
+git push origin gh-pages
+git checkout master
