d2607a17c78d4e102edaf57d79d8292ee4e4fb1c
Eastman Jian <50935@qq.com>
2017-05-06 01:49:01 +0800

    deploy blog

--- sync_src_to_vps.sh
+++ sync_src_to_vps.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -v
+PORT=28411
 
-#pack and ship the blog source to VPS
-cd ..
-tar cvfz eastman_blog.tar.gz eastman_blog/
-scp -P 28411 eastman_blog.tar.gz eastman@45.78.51.215:/home/eastman
-rm eastman_blog.tar.gz
-ssh -p 28411 eastman@45.78.51.215 << EOF 
-  cd /home/eastman
-  rm -rf eastman_blog/
-  tar xvfz eastman_blog.tar.gz
-  rm eastman_blog.tar.gz
-EOF
-cd -
+#sync the blog source to VPS
+rsync -zrtopgv -e "ssh -p $PORT" --delete --progress --exclude "_site" ./* eastman@eastmanjian.cn:/home/eastman/eastman_blog/
