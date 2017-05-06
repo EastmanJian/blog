a67c8dff6277c559aa31209d9a36e8302388320f
Eastman Jian <50935@qq.com>
2017-05-06 23:30:34 +0800

    centralized vps parameters to _config.yml, improved deployment scripts

--- deploy_vps.sh
+++ deploy_vps.sh
@@ -1,9 +1,24 @@
 #!/bin/sh -v
-PORT=28411
+#Deploy the Jekyll generated _site files to the VPS host's web server document root
+#The client public key should be placed in the .ssh/authorized_keys for connection establishment.
+#Use --images or -i option to sync images files from jekyll source folder only.
+
+
+#Get the parameters from _config.yml
+HOST=$(grep 'vps_host:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+PORT=$(grep 'vps_ssh_port:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+USERNAME=$(grep 'vps_ssh_username:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+DES=$(grep 'vps_htdocs_root:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+BASEURL=$(grep 'baseurl:' _config.yml | awk 'BEGIN{FS="\""}{print $2}')
 
 #build blog _site files
 jekyll build
 
-#sync _site files to VPS
-rsync -zrtopgv -e "ssh -p $PORT" --delete --progress ./_site/* eastman@eastmanjian.cn:/srv/www/htdocs/blog/
+#sync _site files or images to VPS
+if [ "$1" = "--images" -o "$1" = "-i" ] ; then
+  rsync -zrtopgv -e "ssh -p $PORT" --delete --progress ./images $USERNAME@$HOST:$DES$BASEURL
+else
+  rsync -zrtopgv -e "ssh -p $PORT" --delete --progress ./_site/* $USERNAME@$HOST:$DES$BASEURL
+fi
+
 
--- sync_src_to_vps.sh
+++ sync_src_to_vps.sh
@@ -1,5 +1,12 @@
-#!/bin/sh -v
-PORT=28411
+#!/bin/sh -x
+#sync the jekyll site source to the target host.
+#The client public key should be placed in the .ssh/authorized_keys for connection establishment.
+
+#Get the parameters from _config.yml
+HOST=$(grep 'vps_host:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+PORT=$(grep 'vps_ssh_port:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+USERNAME=$(grep 'vps_ssh_username:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
+DES=$(grep 'vps_jekyll_site:' _config.yml | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^ *//g')
 
 #sync the blog source to VPS
-rsync -zrtopgv -e "ssh -p $PORT" --delete --progress --exclude "_site" ./* eastman@eastmanjian.cn:/home/eastman/eastman_blog/
+rsync -zrtopgv -e "ssh -p $PORT" --delete --progress --exclude "_site" ./* $USERNAME@$HOST:$DES
